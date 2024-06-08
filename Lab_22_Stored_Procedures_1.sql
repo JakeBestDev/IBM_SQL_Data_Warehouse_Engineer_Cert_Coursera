ceeccdb6f8bd12f0a78f5780fb5a193066077992
JakeBestDev <jakebestdev708@gmail.com>
2024-06-08 16:27:09 -0500

    Stored Procedures Example 1

---
+++ Lab_22_Stored_Procedures_1.sql
@@ -0,0 +1,32 @@
+-- Stored Procedures EXAMPLE 1 --
+-- Uses the PETSALE Data --
+
+
+-- 1) Exercise 1 - You will create a stored procedure routine named RETRIEVE_ALL.
+;
+    --#SET TERMINATOR @
+CREATE PROCEDURE RETRIEVE_ALL       -- Name of this stored procedure routine
+
+LANGUAGE SQL                        -- Language used in this routine 
+READS SQL DATA                      -- This routine will only read data from the table
+
+DYNAMIC RESULT SETS 1               -- Maximum possible number of result-sets to be returned to the caller query
+
+BEGIN 
+
+ DECLARE C1 CURSOR               -- CURSOR C1 will handle the result-set by retrieving records row by row from the table
+ WITH RETURN FOR                 -- This routine will return retrieved records as a result-set to the caller query
+        
+  SELECT * FROM PETSALE;          -- Query to retrieve all the records from the table
+        
+  OPEN C1;                        -- Keeping the CURSOR C1 open so that result-set can be returned to the caller query
+
+END
+@                               -- Routine termination character
+
+
+
+CALL RETRIEVE_ALL     -- Caller query
+
+-- Used To Drop Procedure - 
+-- DROP PROCEDURE RETRIEVE_ALL
